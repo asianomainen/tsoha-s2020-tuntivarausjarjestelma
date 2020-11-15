bdf329afc3433e1ff540c88ca973a9c8c15e22ae
asianomainen <oskari.nuottonen@gmail.com>
2020-11-15 17:03:31 +0200

    Update SQL schema

--- schema.sql
+++ schema.sql
@@ -2,9 +2,10 @@ CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
 	username TEXT UNIQUE,
 	password TEXT,
-	phone TEXT,
 	first_name TEXT,
 	last_name TEXT,
+	email TEXT,
+	phone TEXT,
 	admin INTEGER DEFAULT 0
 );
 
@@ -20,11 +21,6 @@ CREATE TABLE lessons (
 CREATE TABLE sign_ups (
 	id SERIAL PRIMARY KEY,
 	lesson_id INTEGER REFERENCES lessons(id),
-	user_id INTEGER REFERENCES users(id)
-);
-
-CREATE TABLE reserve (
-	id SERIAL PRIMARY KEY,
-	lesson_id INTEGER REFERENCES lessons(id),
-	user_id INTEGER REFERENCES users(id)
+	user_id INTEGER REFERENCES users(id),
+	reserve INTEGER DEFAULT 0
 );
