c03de2ba783e9519436b937aefc21044275653a4
asianomainen <oskari.nuottonen@gmail.com>
2020-11-15 16:57:46 +0200

    Add SQL schema

---
+++ schema.sql
@@ -0,0 +1,30 @@
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	username TEXT UNIQUE,
+	password TEXT,
+	phone TEXT,
+	first_name TEXT,
+	last_name TEXT,
+	admin INTEGER DEFAULT 0
+);
+
+CREATE TABLE lessons (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	spots INTEGER,
+	date DATE,
+	start TIME,
+	duration INTEGER
+);
+
+CREATE TABLE sign_ups (
+	id SERIAL PRIMARY KEY,
+	lesson_id INTEGER REFERENCES lessons(id),
+	user_id INTEGER REFERENCES users(id)
+);
+
+CREATE TABLE reserve (
+	id SERIAL PRIMARY KEY,
+	lesson_id INTEGER REFERENCES lessons(id),
+	user_id INTEGER REFERENCES users(id)
+);
